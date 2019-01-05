be9a000ad8495762e8854034a463332eda21e670
stqc <42732801+stqc@users.noreply.github.com>
2019-01-05 19:20:53 +0530

    Update linear regression from scratch.py

--- linear regression from scratch.py
+++ linear regression from scratch.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x=np.array([1,2,3,4,5,6],dtype=np.float64)
-y=np.array([5,4,6,5,6,7],dtype=np.float64)
+x=np.array([1,2,3,4,5,6,7],dtype=np.float64)
+y=np.array([6,7,6,3,6,7,9],dtype=np.float64)
 
 
 def best_fit_slope_and_intercept(a,b):
